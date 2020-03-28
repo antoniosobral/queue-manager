449497019ebb99512cbb6d3f59fd06dcfa37198c
Antonio Sobral <antoniosobral@poli.ufrj.br>
2020-03-28 14:12:38 -0300

    deploy final version

--- src/app/controllers/MailController.js
+++ src/app/controllers/MailController.js
@@ -7,7 +7,7 @@ class MailController {
     let destinatario = '';
 
     if (subject === 'DÚVIDA') {
-      destinatario = 'antoniooosobral@poli.ufrj.br';
+      destinatario = 'antoniosobral@poli.ufrj.br';
     } else {
       destinatario = 'direcaosobral@gmail.com';
     }
